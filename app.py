486ee8c05f3bc8cba5fe291007e34b402a574d47
ElianHuanca <huancacori@gmail.com>
2025-06-24 14:10:11 -0400

    pequeno cambios

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from routes.dashboard import dashboard_bp
 from routes.insights import insights_bp
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(productos_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(insights_bp)
 
 if __name__ == '__main__':
-    CORS(app) 
     app.run(debug=True, host='0.0.0.0', port=5000)  # Acepta conexiones externas si es necesario
     
